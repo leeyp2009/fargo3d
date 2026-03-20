d948a653ac2dfe3f98b8c84ca8876639971ab8db
Yaping Li <leeyp2009@gmail.com>
2026-03-20 19:54:33 +0800

    Update planets.c

--- src/planets.c
+++ src/planets.c
@@ -244,7 +244,6 @@ void AdvanceSystemFromDisk(real dt) {
       Sys->vz[k] += dt * IndirectTerm.z;
 #endif
     } else if ((Sys->FeelDisk[k] == 0) && (Sys->Flag_Pres[k]==YES)) {
-	printf("this is a test!");
     UpdatePlanetFromTrajectory(Sys, PhysicalTime, k);
 	}
   }
